72ad6c5a3be7840bfa9e785bcc6453aa1a3d2ca4
sam <2214963202@qq.com>
2024-08-13 14:59:06 +0100

    test

---
+++ trainS2.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python3 -m torch.distributed.launch --nproc_per_node=8 --master_port=4392 DiffIR/train.py -opt options/train_DiffIRS2.yml --launcher pytorch
+
+CUDA_VISIBLE_DEVICES=0 python3 DiffIR/train.py -opt options/train_DiffIRS2.yml
